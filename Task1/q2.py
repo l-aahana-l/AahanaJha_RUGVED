97b675ab3fff7a4f3253af48f1976a09d424f2ad
Aahana <aahanajha26@gmail.com>
2025-09-23 16:08:26 +0530

    Update q2.py

--- Task1/q2.py
+++ Task1/q2.py
@@ -1,1 +1,9 @@
-
+input_string = input("Input the String: ")
+sorted_string = "".join(sorted(input_string))
+print("Sorted String:",sorted_string)
+count = {}
+for char in sorted_string:
+    count[char]=count.get(char,0)+1
+print("Character count:")
+for char in sorted(count):
+    print(char,":",count[char])
