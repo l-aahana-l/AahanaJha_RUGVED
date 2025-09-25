3957a249d505dda944ad3126a863a19d67ce4b8b
Aahana <aahanajha26@gmail.com>
2025-09-25 13:10:59 +0530

    Update q3.py

--- Task1/q3.py
+++ Task1/q3.py
@@ -1,1 +1,19 @@
+def isHillNum(n):
+    digits = [int(d) for d in str(n)]
+    length = len(digits)
+    if length < 3:
+        return False
+    i = 1
+    while i < length and digits[i] > digits[i - 1]:
+        i += 1
+    if i == 1 or i == length:
+        return False
+    while i < length and digits[i] < digits[i - 1]:
+        i += 1
 
+    return i == length
+num = int(input("Enter a number: "))
+if isHillNum(num):
+    print("Hill number")
+else:
+    print("Not a hill number")
